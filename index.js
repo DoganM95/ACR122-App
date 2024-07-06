4e614ea5b9591fc797cc109cb1249cf2f6cb128a
DoganM95 <doganmermertas@hotmail.de>
2024-07-06 19:11:22 +0200

    promisify commands and prepare cardInfo object

--- index.js
+++ index.js
@@ -3,64 +3,93 @@ const pcsc = pcsclite();
 
 console.log("Looking for a reader device...");
 
-// Commands specific to acr122u
+// Commands specific to ACR122U
 const GET_UID = Buffer.from([0xff, 0xca, 0x00, 0x00, 0x00]);
 const GET_ATR = Buffer.from([0xff, 0xca, 0x00, 0x00, 0x00]); // Placeholder for ATR command
-const READ_SECTOR = (sector) => Buffer.from([0xff, 0xb0, 0x00, sector * 4, 16]);
+const READ_SECTOR = (sector) => Buffer.from([0xff, 0xb0, 0x00, sector * 4, 16]); // Command to read sector
 const GET_FIRMWARE_VERSION = Buffer.from([0xff, 0x00, 0x48, 0x00, 0x00]);
 const GET_PICC_OPERATING_PARAMETERS = Buffer.from([0xff, 0x00, 0x50, 0x00, 0x00]);
 const GET_READER_STATUS = Buffer.from([0xff, 0x00, 0x64, 0x00, 0x00]);
 
-let cardUid;
-let cardData;
+let cardInfo = {
+    uid: null,
+    atr: null,
+    sectors: {},
+    firmwareVersion: null,
+    piccOperatingParameters: null,
+    readerStatus: null,
+};
 
-pcsc.on("reader", function (reader) {
+pcsc.on("reader", async (reader) => {
     console.log("Reader detected:", reader.name);
 
-    reader.on("error", function (err) {
+    reader.on("error", (err) => {
         console.error("Error(", reader.name, "):", err.message);
     });
 
-    reader.on("status", function (status) {
+    reader.on("status", async (status) => {
         console.log(status.state);
         const changes = reader.state ^ status.state; // bitwise XOR operation
         if (changes) {
             // bitwise AND operation
             if (changes & reader.SCARD_STATE_EMPTY && status.state & reader.SCARD_STATE_EMPTY) {
                 console.log("Card removed");
-                reader.disconnect(reader.SCARD_LEAVE_CARD, function (err) {
-                    if (!err) {
-                        console.log("Disconnected");
-                    } else {
-                        console.error("Disconnect error:", err);
-                    }
-                });
+                await disconnect(reader)
+                    .then(() => console.log("Disconnected."))
+                    .catch((err) => console.error("Disconnect error:", err));
             } else if (changes & reader.SCARD_STATE_PRESENT && status.state & reader.SCARD_STATE_PRESENT) {
                 console.log("Card inserted");
-                reader.connect({ share_mode: reader.SCARD_SHARE_SHARED }, (err, protocol) => {
-                    if (!err) {
-                        console.log("Connected, protocol:", protocol);
-                        reader.transmit(GET_UID, 40, protocol, function (err, data) {
-                            if (!err) {
-                                console.log("Card UID:", data.toString("hex"));
-                                cardUid = data.toString("hex");
-                            } else {
-                                console.error("Transmit error:", err);
-                            }
-                        });
-                    } else {
-                        console.error("Connect error:", err);
-                    }
-                });
+                let protocolReturned;
+                await connect(reader)
+                    .then((protocol) => (protocolReturned = protocol))
+                    .catch((err) => console.error("Failed to retrieve the protocol." + err));
             }
         }
     });
 
-    reader.on("end", function () {
+    reader.on("end", () => {
         console.log("Reader", reader.name, "removed");
     });
 });
 
-pcsc.on("error", function (err) {
+pcsc.on("error", (err) => {
     console.error("PCSC error", err.message);
 });
+
+async function connect(reader) {
+    return new Promise((resolve, reject) => {
+        reader.connect({ share_mode: reader.SCARD_SHARE_SHARED }, function (err, protocol) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected, protocol:", protocol);
+                resolve(protocol);
+            }
+        });
+    });
+}
+
+const disconnect = async (reader) => {
+    return new Promise((resolve, reject) => {
+        reader.disconnect(reader.SCARD_LEAVE_CARD, function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Disconnected");
+                resolve();
+            }
+        });
+    });
+};
+
+const transmit = async (reader, protocol, command) => {
+    return new Promise((resolve, reject) => {
+        reader.transmit(command, 40, protocol, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
