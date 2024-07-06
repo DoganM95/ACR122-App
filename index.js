f6b522124167308af16f92f894e8d7c594142ca1
DoganM95 <doganmermertas@hotmail.de>
2024-07-06 18:00:06 +0200

    reafactor code to forst show needed conditions

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const pcsc = pcsclite();
 
 console.log("Looking for a reader device...");
 
+// Commands specific to acr122u
+const GET_UID = Buffer.from([0xff, 0xca, 0x00, 0x00, 0x00]);
+
+let cardUid;
+let cardData;
+
 pcsc.on("reader", function (reader) {
     console.log("Reader detected:", reader.name);
 
@@ -18,20 +24,27 @@ pcsc.on("reader", function (reader) {
             if (changes & reader.SCARD_STATE_EMPTY && status.state & reader.SCARD_STATE_EMPTY) {
                 console.log("Card removed");
                 reader.disconnect(reader.SCARD_LEAVE_CARD, function (err) {
-                    if (err) {
-                        console.error("Disconnect error:", err);
-                    } else {
+                    if (!err) {
                         console.log("Disconnected");
+                    } else {
+                        console.error("Disconnect error:", err);
                     }
                 });
             } else if (changes & reader.SCARD_STATE_PRESENT && status.state & reader.SCARD_STATE_PRESENT) {
                 console.log("Card inserted");
                 reader.connect({ share_mode: reader.SCARD_SHARE_SHARED }, function (err, protocol) {
-                    if (err) {
-                        console.error("Connect error:", err);
-                    } else {
+                    if (!err) {
                         console.log("Connected, protocol:", protocol);
-                        readCard(reader, protocol);
+                        reader.transmit(GET_UID, 40, protocol, function (err, data) {
+                            if (!err) {
+                                console.log("Card UID:", data.toString("hex"));
+                                cardUid = data.toString("hex");
+                            } else {
+                                console.error("Transmit error:", err);
+                            }
+                        });
+                    } else {
+                        console.error("Connect error:", err);
                     }
                 });
             }
@@ -46,14 +59,3 @@ pcsc.on("reader", function (reader) {
 pcsc.on("error", function (err) {
     console.error("PCSC error", err.message);
 });
-
-function readCard(reader, protocol) {
-    const GET_UID = Buffer.from([0xff, 0xca, 0x00, 0x00, 0x00]);
-    reader.transmit(GET_UID, 40, protocol, function (err, data) {
-        if (err) {
-            console.error("Transmit error:", err);
-        } else {
-            console.log("Card UID:", data.toString("hex"));
-        }
-    });
-}
