3c7e3df99cf6814f0d931f37238940e21e423f9f
DoganM95 <doganmermertas@hotmail.de>
2024-07-06 19:58:17 +0200

    add some keys to try and an authentication function

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ const GET_PICC_OPERATING_PARAMETERS = Buffer.from([0xff, 0x00, 0x50, 0x00, 0x00]
 const GET_READER_STATUS = Buffer.from([0xff, 0x00, 0x64, 0x00, 0x00]);
 const GET_UID = Buffer.from([0xff, 0xca, 0x00, 0x00, 0x00]);
 
-const AUTHENTICATE = (sector) => Buffer.from([0xff, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, sector * 4, 0x60, 0x00]);
-const READ_BLOCK = (block) => Buffer.from([0xff, 0xb0, 0x00, block, 0x10]);
+const READ_SECTOR = (sector) => Buffer.from([0xff, 0xb0, 0x00, sector * 4, 16]);
 
-const DEFAULT_KEY = Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+const DEFAULT_KEYS = [
+    Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]), // Default key
+    Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5]), // Key A
+    Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5]), // Key B
+    Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0x4d, 0x3a, 0x99, 0xc3, 0x51, 0xdd]), // Transport key
+    Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0x1a, 0x98, 0x2c, 0x7e, 0x45, 0x9d]), // Custom key 1
+    Buffer.from([0xff, 0x82, 0x00, 0x00, 0x06, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]), // Custom key 2
+];
 
 let cardInfo = {
     uid: null,
@@ -42,9 +48,9 @@ pcsc.on("reader", async (reader) => {
                     .catch((err) => console.error("Disconnect error:", err));
             } else if (changes & reader.SCARD_STATE_PRESENT && status.state & reader.SCARD_STATE_PRESENT) {
                 console.log("Card inserted");
-                let protocol;
+                let protocolReturned;
                 await connect(reader)
-                    .then((proto) => (protocol = proto))
+                    .then((protocol) => (protocolReturned = protocol))
                     .catch((err) => console.error("Failed to retrieve the protocol." + err));
                 try {
                     cardInfo.uid = (await transmit(reader, protocol, GET_UID)).toString("hex");
@@ -120,28 +126,40 @@ const transmit = async (reader, protocol, command) => {
     });
 };
 
+const authenticate = async (reader, protocol, sector, key) => {
+    const command = Buffer.concat([Buffer.from([0xff, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, sector * 4, 0x60, 0x00]), key]);
+    return transmit(reader, protocol, command);
+};
+
 const readAllSectors = async (reader, protocol) => {
     let sector = 0;
     while (true) {
-        try {
-            await transmit(reader, protocol, DEFAULT_KEY);
-            await transmit(reader, protocol, AUTHENTICATE(sector));
+        let authenticated = false;
+        for (let key of DEFAULT_KEYS) {
+            try {
+                await authenticate(reader, protocol, sector, key);
+                authenticated = true;
+                break;
+            } catch (err) {
+                console.error(`Authentication failed for sector ${sector} with key ${key.toString("hex")}:`, err.message);
+            }
+        }
+        if (!authenticated) {
+            console.error(`Failed to authenticate sector ${sector}`);
+            break;
+        }
 
-            for (let block = sector * 4; block < sector * 4 + 4; block++) {
-                const command = READ_BLOCK(block);
+        for (let block = sector * 4; block < sector * 4 + 4; block++) {
+            try {
+                const command = READ_SECTOR(block);
                 const data = await transmit(reader, protocol, command);
                 cardInfo.sectors[`block${block}`] = data.toString("hex");
                 console.log(`Block ${block} data:`, data.toString("hex"));
-            }
-            sector++;
-        } catch (err) {
-            if (err.message.includes("SW1/SW2")) {
-                console.log("No more sectors to read.");
-                break;
-            } else {
-                console.error(`Error reading sector ${sector}:`, err.message);
+            } catch (err) {
+                console.error(`Error reading block ${block}:`, err.message);
                 break;
             }
         }
+        sector++;
     }
 };
